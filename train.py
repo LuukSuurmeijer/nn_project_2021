e9e7b3a4368158387abb420d3c66852e61c3e6ca
luuksuurmijer <luuksuurmeijer@gmail.com>
2021-03-16 18:23:45 +0100

    added graph functionality

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 HIDDEN_DIM = 100
 EMBEDDING_DIM = 768
 TAGSET_SIZE = 40
-EPOCHS = 1
+EPOCHS = 15
 
 model = RNNTagger(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, tagset_size=TAGSET_SIZE)
 
@@ -23,20 +23,24 @@ criterion = nn.CrossEntropyLoss(ignore_index=-100) #ignore padding ?
 d = datasets.load_dataset('TagDataset.py', data_dir='train_test_split/')
 
 #tokenize the data
+print("Generating embeddings...")
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True, is_split_into_words=True)
 embedding_model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True)
 
 #dataset
+print("Tokenizing data...")
 label_to_id = get_label2id_list(d['train'], 'tags')
 tokenized_train = tokenize_and_align_labels(label_to_id, tokenizer, d['train'], 'words', 'tags')
 
+examples = tokenized_train['input_ids']
 
 train_losses = []
 train_counter = []
+avg_epoch_loss = []
 for epoch in range(EPOCHS):
     running_loss = 0.0
     model.train()
-    for id, example in enumerate(tokenized_train['input_ids'][0:25]):
+    for id, example in enumerate(tokenized_train['input_ids']):
         #model.initHidden()
         input = create_embeddings(embedding_model, tokenized_train['input_ids'][id]) #shape: (1, 86, 768)
         target = tokenized_train['labels'][id]
@@ -58,12 +62,16 @@ for epoch in range(EPOCHS):
         # print statistics
         running_loss += loss.item()
         train_losses.append(loss.item())
-        train_counter.append(id*(epoch+1))
-        print(f"Epoch: {epoch+1} | example: {id} | loss: {loss.item()}")
+        train_counter.append((epoch*len(examples)) + id)
+        if id % 41 == 0:
+            print(f"Epoch: {epoch+1} | example: {id}/{len(examples)} | loss: {loss.item()}")
 
-    print(f"Loss after epoch {epoch}: {running_loss}")
+    print(f"Loss after epoch {epoch+1}: {running_loss}")
+    avg_epoch_loss.append(running_loss/len(examples))
+
 
 plt.plot(train_counter, train_losses, color='blue')
-plt.xlabel('number of training examples seen')
-plt.ylabel('negative log likelihood loss')
+plt.scatter(list([i * len(examples) for i in range(EPOCHS)]), avg_epoch_loss, color='red')
+plt.xlabel('Number of training examples seen')
+plt.ylabel('Cross Entropy Loss')
 plt.show()
