f07cef5d5ee1bc9af02f3d462293b19f75a6e42d
annalena <annalenakohnert@gmail.com>
2021-03-23 13:20:00 +0100

    fix conll preprocessing

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -1,83 +1,44 @@
-import conllu
-import re
 import argparse
 import os
 from collections import Counter
 
-# clean the file because conllu.parse function needs id as first column, "_" and not "-" as None values and no floats in 'head' column
-def clean_file(infile,outfile):
-    delete_list = ["nw/xinhua/02/chtb_0223   0   ", "nw/xinhua/02/chtb_0223   0    ", "bc/phoenix/00/phoenix_0001   0    ",
-                   "bc/phoenix/00/phoenix_0001   0   ", "bc/phoenix/00/phoenix_0001   1    ",
-                   "bc/phoenix/00/phoenix_0001   1   ", "bc/phoenix/00/phoenix_0001   2    ", "bc/phoenix/00/phoenix_0001   2   ",
-                   "bc/phoenix/00/phoenix_0001   3    ", "bc/phoenix/00/phoenix_0001   3   ", "bc/phoenix/00/phoenix_0001   4    ",
-                   "bc/phoenix/00/phoenix_0001   4   ", "bc/phoenix/00/phoenix_0001   5    ", "bc/phoenix/00/phoenix_0001   5   ",
-                   "bc/phoenix/00/phoenix_0001   6    ", "bc/phoenix/00/phoenix_0001   6   ", "bn/pri/00/pri_0016   0    ",
-                   "bn/pri/00/pri_0016   0   ", "nw/wsj/16/wsj_1681   0    ", "nw/wsj/16/wsj_1681   0   "]
-
+def get_sentences(infile, outfile):
     with open(infile) as fin, open(outfile, "w+") as fout:
+        tags = Counter()
+        sentences = []
+        sentence = []
         for line in fin:
-            for word in delete_list:
-                line = line.replace(word, "")
-                line = line.replace(" - "," _ ")
-                line = re.sub(r"([0-9]+)\.([0-9]+)", "_", line)
-            fout.write(line)
-
-# returns a list pf TokenLists, each TokenList is a sentence
-# each item in a token list has a corresponding dictionary storing information e.g.
-# {'id': 0, 'form': 'Xinhua', 'lemma': 'NNP', 'upos': '(TOP(FRAG(NP*', 'xpos': None, 'feats': None, 'head': None, 'deprel': '_', 'deps': '(ORG*', 'misc': None}
-# in our case keys are not named correctly because the columns do not have the correct order (e.g. the key "lemma" actually stores the POS tag)
-# for more information see: https://pypi.org/project/conllu/
-def parse_conll_file(file):
-    with open(file) as f:
-        data = f.read()
-    sentences = conllu.parse(data)
-    # have to drop the last element of the sentence list because it is for whatever reason just an empty TokenList
-    sentences.pop(-1)
-    return sentences
+            if line[0] != '#' and line != '\n':
+                index = line.split()[2]
+                word = line.split()[3]
+                POS = line.split()[4]
+                fout.write(index + '\t' + word + '\t' + POS + '\n')
+                tags[POS] += 1
+                sentence.append(word)
+            if line == '\n':
+                fout.write('\n')
+                sentences.append(sentence)
+                sentence = []
+    return tags, sentences
 
 
-def get_number_of_sentences(sentences):
-    return len(sentences)
+def get_info(tags, sentences, outfile):
+    with open(outfile, "w+") as fout:
+        fout.write(f'Number of sentences: {len(sentences)}\n')
+        sentence_length = [len(elem) for elem in sentences]
+        max_sent = max(sentence_length)
+        min_sent = min(sentence_length)
+        avg_sent = sum(sentence_length) / len(sentences)
 
-# get a list which stores at index 0 the length of sentence 0 and so on
-def get_sentence_lengths(sentences):
-    sentence_lengths = []
-    for sentence in sentences:
-        sentence_length = len(sentence)
-        sentence_lengths.append(sentence_length)
-    return sentence_lengths
+        fout.write(f'Maximum sequence length: {max_sent}\n')
+        fout.write(f'Minimum sequence length: {min_sent}\n')
+        fout.write(f'Average sequence length: {avg_sent}\n')
 
-
-def get_sentence_length_stats(sentence_lengths):
-    average_sen_len = sum(sentence_lengths) / len(sentence_lengths)
-    return max(sentence_lengths), min(sentence_lengths), average_sen_len
-
-
-def create_tab_file(sentences, outfile):
-    with open(outfile, "w+") as f:
-        for sentence in sentences:
-            for word in sentence:
-                f.write(f"{word['id']}\t{word['form']}\t{word['lemma']}\n")
-            f.write("*\n")
-
-
-def get_information_about_data(sentences, filename):
-    sentence_lengths = get_sentence_lengths(sentences)
-    min, max, av, = get_sentence_length_stats(sentence_lengths)
-    list_of_tags = []
-    for sentence in sentences:
-        for word in sentence:
-            list_of_tags.append(word['lemma'])
-    c = Counter(list_of_tags)
-    number_of_tags = len(list_of_tags)
-    with open(filename,"w+") as f:
-        f.write(f"Maximum sequence length: {min}\n"
-                f"Minimum sequence length: {max}\n"
-                f"Mean sequence length: {av}\n"
-                f"Number of sentences: {get_number_of_sentences(sentences)}\n")
-        f.write("\nTags:\n")
-        for tag in Counter(list_of_tags).keys():
-            f.write(f"{tag}\t{round(c[tag] / number_of_tags * 100, 2)}%\n")
+        fout.write('\nTags\n')
+        tags_total = sum(tags.values())
+        for k, v in tags.items():
+            percentage = v * 100.0 / tags_total
+            fout.write(f'{k}\t{round(percentage,2)}%\n')
 
 
 def main():
@@ -88,14 +49,12 @@ def main():
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
-    outfile = "cleaned.conll"
-    tsv_file = "sample.tsv"
-    info_file = "sample.info"
+    tsv_file = "data.tsv"
+    info_file = "data.info"
 
-    clean_file(args.input_file, os.path.join(args.output_dir, outfile))
-    sentences = parse_conll_file(os.path.join(args.output_dir,outfile))
-    create_tab_file(sentences, os.path.join(args.output_dir,tsv_file))
-    get_information_about_data(sentences, os.path.join(args.output_dir,info_file))
+    tags, sentences = get_sentences(args.input_file, args.output_dir+tsv_file)
+    get_info(tags, sentences, args.output_dir+info_file)
+
 
 if __name__ == '__main__':
     main()
